37952886c5e4c717698068aae93033dfb394ad2a
Fabio <islerfab@gmail.com>
2016-12-29 16:59:59 +0100

    added a simple log

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,9 +11,11 @@ import {Platform} from 'ionic-angular';
 })
 export class HomePage {
   private speed: number;
+  private log: string[];
 
   constructor(public navCtrl: NavController, public speedService: SpeedService, public platform: Platform) {
     this.speed = 0;
+    this.log = [];
   }
 
   changeSpeed(speed) {
@@ -21,12 +23,13 @@ export class HomePage {
     this.speedService.setSpeed(speed)
       .then(response => {
         console.log('response', response);
-
+        this.log.unshift('Success: ' + response.toString());
       })
       .catch(error => {
         console.log('Error: ', error.toString());
+        this.log.unshift('Error: ' + error.toString());
         if (this.platform.is('android') || this.platform.is('ios') || this.platform.is('windows')) {
-          Toast.show('Error: ' + error, '5000', 'center').subscribe(
+          Toast.show('Error: ' + error, '5000', 'bottom').subscribe(
             toast => {
               console.log('Toast Success', toast);
             },
