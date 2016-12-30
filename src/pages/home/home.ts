9bf7293c8c94920f4bcfc8aef81b2c528ae9de66
Fabio <islerfab@gmail.com>
2016-12-30 02:44:28 +0100

    added a simple random option

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {SpeedService} from "../../providers/speed-service";
-import {Toast} from 'ionic-native';
+import {ToastController} from 'ionic-angular';
 import {Platform} from 'ionic-angular';
 
 @Component({
@@ -12,10 +12,15 @@ import {Platform} from 'ionic-angular';
 export class HomePage {
   private speed: number;
   private log: string[];
+  private random: boolean;
+  private interval: any;
+  private showLog: boolean;
 
-  constructor(public navCtrl: NavController, public speedService: SpeedService, public platform: Platform) {
+  constructor(public navCtrl: NavController, public speedService: SpeedService, public platform: Platform, public toastCtrl: ToastController) {
     this.speed = 0;
     this.log = [];
+    this.random = false;
+    this.showLog = false;
   }
 
   changeSpeed(speed) {
@@ -26,22 +31,44 @@ export class HomePage {
         this.log.unshift('Success: ' + response.toString());
       })
       .catch(error => {
-        console.log('Error: ', error.toString());
+        console.log('Error: ', error);
         this.log.unshift('Error: ' + error.toString());
-        if (this.platform.is('android') || this.platform.is('ios') || this.platform.is('windows')) {
-          Toast.show('Error: ' + error, '5000', 'bottom').subscribe(
-            toast => {
-              console.log('Toast Success', toast);
-            },
-            error => {
-              console.log('Toast Error', error);
-            },
-            () => {
-              console.log('Toast Completed');
-            }
-          )
+        if (this.random) {
+          if (this.interval) {
+            clearInterval(this.interval);
+            console.log("cleared interval");
+          }
+          this.random = false;
         }
+        let toast = this.toastCtrl.create({
+          message: 'Error: ' + error,
+          duration: 3000,
+          position: 'bottom'
+        });
+        toast.present()
+          .catch(err => console.log(err));
       })
+  }
+
+  triggerRandom(event) {
+    console.log(event);
+    if (this.random) {
+      if (this.interval) {
+        clearInterval(this.interval);
+        console.log("cleared interval");
+      }
+      this.random = false;
+    } else {
+      let that = this;
+      this.interval = setInterval(function () {
+        let newSpeed = that.speed;
+        if (newSpeed < 0) newSpeed = Math.round(Math.random() * 50);
+        else newSpeed = Math.min(that.speed + Math.round(Math.random() * 40 - 20), 50);
+        that.speed = newSpeed;
+        that.changeSpeed(that.speed);
+      }, 5000);
+      this.random = true;
+    }
   }
 
 }
